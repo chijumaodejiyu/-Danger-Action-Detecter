6c6ec72fbf822fd00e4b961877e8f5a098492e1e
CJMDJY <2077987156@qq.com>
2024-01-03 13:29:50 +0800

    cjmdjy initialized his project

---
+++ modules/base.py
@@ -0,0 +1,16 @@
+def init(data: dict) -> dict:
+    """
+    利用data初始化并以字典形式返回的所需变量
+    :param data: 以字典形式储存的所需数据
+    :return: 以字典形式返回的所需变量（包含：'servo', 'capture' 等）
+    """
+    pass
+
+
+def get_image(capture) -> np.ndarray:
+    """
+    从指定摄像头中获取图片并返回
+    :param capture: 指定的摄像头
+    :return: 从摄像头中获取的图片
+    """
+    pass
