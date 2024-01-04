42a6f5ef1370de9a862e4b9effc58584b22dbbe7
CJMDJY <2077987156@qq.com>
2024-01-04 12:43:17 +0800

    cjmdjy has perfected the method of obtaining the camera list

--- modules/Visual_threads.py
+++ modules/Visual_threads.py
@@ -6,8 +6,22 @@ from copy import deepcopy
 import threading
 from PyCameraList.camera_device import list_video_devices
 
-thread_lock = threading.Lock()
-thread_exit = False
+thread_lock = threading.Lock()  # 全局线程锁
+thread_exit = False  # 定义全局变量thread_exit,标志主线程退出，初始化False
+
+
+def get_cap_list():
+    """
+        调用PyCameraList获取可用摄像头的名称及id
+        :param None
+        :return: 以字典形式返回所有的摄像头信息（eg. {0: 'Intel(R) RealSense(TM) 3D Camera (Front F200) RGB', 1: 'NewTek NDI Video'}））
+    """
+    cameras = list_video_devices()
+    camera_num = len(cameras)
+    print('可用摄像头数量：' + str(camera_num) + "个\n")
+    for i in range(0, camera_num, 1):
+        print(str(i) + '----' + str(cameras[i]))
+    return cameras
 
 
 class Visual(threading.Thread):
@@ -18,11 +32,11 @@ class Visual(threading.Thread):
         self.img_width = img_width
         self.frame = np.zeros((img_height, img_width, 3), dtype=np.uint8)
 
-    def get_cap_list(self):
-        cameras = list_video_devices()
-        return cameras
-
     def get_frame(self):
+        """
+        获取视频推流的接口方法
+        :return: self.frame的重置大小后的摄像头画面
+        """
         return deepcopy(self.frame)
 
     def run(self):
@@ -40,32 +54,41 @@ class Visual(threading.Thread):
         cap.release()
 
 
-class CapWide:
+"""
+class CapWide(Visual):
     def __init__(self):
+        super().__init__()
         pass
+"""
+
+
+#  暂时无用的cap子类
 
 
 def test():
+    """
+    Visual类的测试方法
+    :return: 测试画面显示窗口
+    """
     global thread_exit
     camera_id = 0
     img_height = 480
     img_width = 640
+    cap_list = get_cap_list()
+    print(cap_list)
     thread = Visual(camera_id, img_height, img_width)
     thread.start()
 
     while not thread_exit:
-            thread_lock.acquire()
-            frame = thread.get_frame()
-            thread_lock.release()
+        thread_lock.acquire()
+        frame = thread.get_frame()
+        thread_lock.release()
 
-            cv2.imshow('Video', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                thread_exit = True
+        cv2.imshow('Video', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            thread_exit = True
     thread.join()
 
 
 if __name__ == "__main__":
     test()
-
-
-
